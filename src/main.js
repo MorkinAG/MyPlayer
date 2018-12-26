d5c8294e639dd6e9f41047e64f1c65640727613a
KirizakiKnad <am@qaq.gd>
2018-12-26 15:31:24 +0800

    安装配置axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import  Axios from "axios";
+
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
+Vue.prototype.HOST = "/baidu_music_api";
+Vue.prototype.$axios = Axios;
 
 new Vue({
   el: '#app',
